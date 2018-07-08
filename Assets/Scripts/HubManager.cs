abf823920a498da050248c27a0c3bfada2d99a68
StreinGon <artyom199788@yandex.by>
2018-07-08 09:14:05 +0300

    project

---
+++ Assets/Scripts/HubManager.cs
@@ -0,0 +1,103 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HubManager : MonoBehaviour {
+
+    public Button create;
+    public Button refresh;
+
+    public InputField room_name;
+
+    private List<GameObject> rooms;
+    public GameObject roomPrefab;
+    public GameObject roomsArea;
+
+    public Text output;
+
+    private static string roomNameJoin;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject.transform);
+        DontDestroyOnLoad(output);
+    }
+
+    private void Start()
+    {       
+        PhotonNetwork.ConnectUsingSettings("1.0");
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.playerName = GameObject.FindWithTag("Nick").GetComponent<TextLine>().getNick();
+        output.text = PhotonNetwork.playerName;
+        refresh.onClick.AddListener(ResfreshRooms);
+        create.onClick.AddListener(CreateRoom);
+        rooms = new List<GameObject>();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("\n\n\nStatus:\n" + PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void CreateRoom()
+    {
+        if (room_name.text.Length > 0)
+        {
+            roomNameJoin = room_name.text;
+            PhotonNetwork.CreateRoom(room_name.text, new RoomOptions(), TypedLobby.Default);
+        }
+    }
+
+    void ResfreshRooms()
+    {
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            Destroy(rooms[i]);
+        }
+        rooms.Clear();
+        RoomsUpdate();
+    }
+
+    void RoomsUpdate()
+    {
+        for (int i = 0; i < PhotonNetwork.GetRoomList().Length; i++)
+        {
+            GameObject newroom = Instantiate(roomPrefab, roomsArea.transform);
+            newroom.transform.localPosition = new Vector3(0, 260 - 44 * i, 0);
+            newroom.transform.Find("Text").GetComponent<Text>().text = PhotonNetwork.GetRoomList()[i].Name;
+            newroom.transform.Find("count_player").GetComponent<Text>().text = PhotonNetwork.GetRoomList()[i].PlayerCount.ToString();
+            newroom.transform.Find("joim").GetComponent<Button>().onClick.AddListener(delegate { JoinRoom(newroom.transform.Find("joim").GetComponent<Button>().gameObject); });
+
+            rooms.Add(newroom);
+        }
+    }
+
+    void JoinRoom(GameObject join)
+    {
+        roomNameJoin = join.transform.parent.transform.Find("Text").GetComponent<Text>().text;
+        PhotonNetwork.JoinRoom(roomNameJoin);
+    }
+
+    public string getCurrenRoomName()
+    {
+        return roomNameJoin;
+    }
+
+    void OnJoinedLobby()
+    {
+
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("testPrefab", Vector3.zero, Quaternion.identity, 0);
+        SceneManager.LoadScene("test");       
+    }
+
+    void OnCreatedRoom()
+    {
+        PhotonNetwork.Instantiate("testPrefab", Vector3.zero, Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : Photon.MonoBehaviour {
+	bool muteALL=false;
+    private int players;
+    public Text output;
+    private string text;
+    public Button back;
+	public Button mute;
+    // Use this for initialization
+    void Start () {
+        PhotonNetwork.ConnectUsingSettings("1.0");
+        output.text = PhotonNetwork.playerName;
+        players = PhotonNetwork.countOfPlayersInRooms;
+        back.onClick.AddListener(BackToLobby);
+		mute.onClick.AddListener(MuteALL);
+    }
+
+    private void Update()
+    {
+        
+        text = "\n\nSpeakers:\n";
+        for (int i = 0; i < PhotonNetwork.room.PlayerCount; i++)
+        {
+            text += PhotonNetwork.playerList[i].NickName + "\n";
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("\n\n\nStatus:\n" + PhotonNetwork.connectionStateDetailed.ToString() + text);
+    }
+
+    public void BackToLobby()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.JoinLobby();
+        SceneManager.LoadScene("Hub");
+    }
+	public void MuteALL(){
+		if (!muteALL) {
+			muteALL = true;
+			AudioListener.volume = 0.0f;
+		} 
+		else {
+			muteALL = false;
+			AudioListener.volume = 1;
+		}
+	
+	}
+    void OnJoinedLobby()
+    {
+    }
+
+    void OnJoinedRoom()
+    {
+    }
+}
